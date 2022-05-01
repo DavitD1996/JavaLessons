b45916ac723adc19a6e8bacb7d5bd9b44ee4839c
Davit Sargsyan <sargsyan.davit1996@gmail.com>
2022-05-02 01:16:46 +0400

    The Homework of the fifth lesson

--- src/main/java/Lesson5/Classwork.java
+++ src/main/java/Lesson5/Classwork.java
@@ -2,10 +2,44 @@ package Lesson5;
 
 public class Classwork {
     public static void main(String[]args) {
-       Car c1=new Car("red","colibri",4);
-       System.out.println(c1.color+" is the color");
-       System.out.println(c1.doorsCount+ " the count of doors");
-
-
+        Human Andrew=new Human("Age","Alarbekyan","Man",12);
+        Human Andrew1=new Human("Abe","Alarbekyan","Man",15);
+        Human Andrew2=new Human("Sash","Alarbekyan","Man",-3);
+        Human[]sortedByAge=sortByName(new Human[]{Andrew1,Andrew,Andrew2});
+        printNames(sortedByAge);
+    }
+    public static Human[] sortByAge(Human[]data){
+        for(int i=0;i<data.length;i++){
+            for(int j=0;j<data.length-1-i;j++){
+                if(data[j].age>data[j+1].age){
+                    Human perm=data[j];
+                    data[j]=data[j+1];
+                    data[j+1]=perm;
+                }
+            }
+        }
+        return data;
+    }
+    public  static Human[]sortByName(Human[]data){
+        for(int i=0;i<data.length;i++){
+            for(int j=0;j<data.length-1-i;j++){
+                int index=0;
+                while(data[j].name.charAt(index)==data[j+1].name.charAt(index)){
+                    index++;
+                }
+                if(data[j].name.charAt(index)>data[j+1].name.charAt(index)){
+                    Human perm=data[j];
+                    data[j]=data[j+1];
+                    data[j+1]=perm;
+                }
+            }
+        }
+        return data;
+    }
+    public static void printNames(Human[]data){
+        for(int i =0;i<data.length;i++){
+            System.out.printf("%s %s is %d years old and %s",data[i].name,data[i].surname,data[i].age,data[i].gender);
+            System.out.println();
+        }
     }
 }
---
+++ src/main/java/Lesson5/Human.java
@@ -0,0 +1,15 @@
+package Lesson5;
+
+public class Human
+{
+    public String name;
+    public String surname;
+    public String gender;
+    public int age;
+    public Human(String Name,String Surname,String Gender,int Age){
+        name=Name;
+        surname=Surname;
+        gender=Gender;
+        age=Age;
+    }
+}
